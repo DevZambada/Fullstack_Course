6bac371b2b46ed263caf1f73f75441f50840a874
DevZambada <max.zambadac@outlook.com>
2023-03-05 11:21:10 -0600

    Continuing JS basics with DOM manipulation

--- curso-js-practico/script.js
+++ curso-js-practico/script.js
@@ -1,16 +1,15 @@
 
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#buttonCalcular');
+const pResult = document.querySelector('#result')
+const form = document.querySelector('#form');
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-});
+form.addEventListener('submit', sumarInputValues);
 
-h1.innerHTML = 'Patito ';
+function sumarInputValues() {
+    event.preventDefault();
+    const sumaInput = input1.value + input2.value;
+    pResult.innerText = 'Resultado: ' + sumaInput;
+}
